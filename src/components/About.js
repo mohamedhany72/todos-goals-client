4b4b731706a0d7b02accc8a55b55ef40b95fa610
mohamedhany72 <medocapo304@yahoo.com>
2022-10-04 15:44:51 +0200

    remove user state from about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,92 +1,12 @@
-import React, { useState } from "react";
 import Header from "./Header";
 
-import { useSelector, useDispatch } from "react-redux";
-import { addUser, removeUser } from "../redux/user/user";
-
-import { getUser, updateUser } from "../redux/user/userThunck";
-
-import * as UAPI from "../api/usersApi";
-
 const About = () => {
-    const user = useSelector((state) => state.user.user);
-    const errMsg = useSelector((state) => state.user.msg);
-    const dispatch = useDispatch();
-
-    const add = () => {
-        let myUser = {
-            id: "vbvvhv",
-            name: "Mido",
-            email: "mido@mido.com",
-            gender: 1,
-            picurl: "picurl.jpg",
-            isVerified: false
-        };
-        dispatch(addUser(myUser));
-    };
-
-    const [csrf, setCsrf] = useState("");
-    const getCsrf = async () => {
-        const res = await UAPI.getCsrf();
-        if (res.success) {
-            // console.log(csrf)
-            setCsrf(res.csrf);
-        }
-    };
-
-    const update = () => {
-        const name = "Mido Capo";
-        const gender = 1;
-        const picurl = "new_picurl.jpg";
-
-        dispatch(updateUser({ name, gender, picurl, csrf }));
-    };
-
-    const get = () => {
-        dispatch(getUser());
-    };
-
-    const remove = () => {
-        dispatch(removeUser());
-    };
-
     return (
         <>
             <Header />
             <div class="container">
                 <div class="about">
                     <h1>about the appp</h1>
-                    <div>{errMsg && <p>Error: {errMsg}</p>}</div>
-                    <p>
-                        <button class="btn btn-primary" onClick={add}>
-                            add user
-                        </button>
-
-                        <button class="btn btn-primary" onClick={update}>
-                            update user
-                        </button>
-
-                        <button class="btn btn-primary" onClick={get}>
-                            get user
-                        </button>
-
-                        <button class="btn btn-primary" onClick={remove}>
-                            empty user
-                        </button>
-
-                        <button class="btn btn-primary" onClick={getCsrf}>
-                            get csrf
-                        </button>
-                    </p>
-                    <p>
-                        id: {user.id} <br />
-                        name: {user.name} <br />
-                        email: {user.email} <br />
-                        gender: {user.gender} <br />
-                        picurl: {user.picurl} <br />
-                        isVerified: {user.isVerified ? "true" : "false"} <br />
-                        csrf: {csrf}
-                    </p>
                     <p>
                         Lorem ipsum dolor sit, amet consectetur adipisicing
                         elit. Excepturi dicta veritatis nulla, autem laboriosam,
